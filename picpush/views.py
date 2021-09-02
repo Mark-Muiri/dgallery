b963013e0b8796b8793a9d6cac3949222e538d3e
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-02 14:28:41 +0300

    inherited templates through a base.html

--- picpush/views.py
+++ picpush/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse, Http404
 import datetime as dt
 
@@ -60,3 +60,19 @@ def past_days_pics(request,past_date):
         </html>
             '''
     return HttpResponse(html)
+
+def past_days_pics(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(pics_of_day)
+
+    return render(request, 'all-news/past-pics.html', {"date": date})
