279d4edeab8b957a63f80c6e5b62b44f9757fa6a
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-02 14:17:51 +0300

    added 1st dynamic url to show past pics

--- picpush/urls.py
+++ picpush/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
-    path('today/',views.pics_of_day,name='picsToday')
+    path('today/',views.pics_of_day,name='picsToday'),
+    path('archives/<pic_id>/',views.past_days_pics,name='pastPics')
 ]
