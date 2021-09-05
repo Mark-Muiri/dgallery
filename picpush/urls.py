ffc59aa9fb79b8e285d78bbcabbdcb02bba2d761
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-05 23:52:21 +0300

    removed past and todays templates and views to prevent interuption

--- picpush/urls.py
+++ picpush/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.welcome,name = 'welcome'),
-    path('today/',views.pics_of_day,name='picsToday'),
-    path('archives/<pic_id>/',views.past_days_pics,name='pastPics')
+    path('',views.home,name = ''),    
+     path('search/', views.search_results, name = 'search_results')
 ]
--- picpush/views.py
+++ picpush/views.py
@@ -1,78 +1,22 @@
-from django.shortcuts import render, redirect
-from django.http  import HttpResponse, Http404
-import datetime as dt
 
+ 
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Image, Category, Location
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+def home(request):
+    images = Image.objects.all()
+    return render(request, 'welcome.html',{'images':images})
 
-def pics_of_day(request):
-    date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request, 'all-pics/search.html',{"message":message,"images": searched_images})
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-def pics_of_day(request):
-    date = dt.date.today()
-
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Pics for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-
-def past_days_pics(request,past_date):
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Picss for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-
-def past_days_pics(request, past_date):
-
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(pics_of_day)
-
-    return render(request, 'all-news/past-pics.html', {"date": date})
+    else:
+        message = "You haven't searched for any term"
+    return render(request, 'all-pics/search.html',{"message":message})
