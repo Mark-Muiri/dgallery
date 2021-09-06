3784068fadf44e73a692bbaa6a2324060bb3ba0f
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-06 09:05:13 +0300

    search functionality

--- picpush/urls.py
+++ picpush/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home,name = ''),    
-     path('search/', views.search_results, name = 'search_results')
+    path('',views.home,name = 'home'),    
+    path('search/', views.search_results, name = 'search_results')
 ]
